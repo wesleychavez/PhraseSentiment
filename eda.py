fd0fb68f9d4157a992cbd7a86252937996a6afc4
Wesley Chavez <wchavez@Wesleys-Air.home>
2018-08-22 14:42:55 -0700

    Started collecting dataset stats

--- eda.py
+++ eda.py
@@ -1,11 +1,22 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def main():
-    train = pd.read_csv('train.tsv', sep="\t")
-    test = pd.read_csv('test.tsv', sep="\t")
-    print (train.columns)    
-    print (test.columns)    
+    train = pd.read_csv('~/Downloads/train.tsv', sep="\t")
     print (train.dtypes)    
+    print (train.shape)    
+    print (train.describe())
+
+    # Mean number of words per phrase
+    print (np.mean(train['Phrase'].apply(lambda x: len(x.split()))))
+
+    # Save a histogram of the sentiments
+    fig, ax = plt.subplots()
+    train.hist('Sentiment', ax=ax, bins=[-0.5,0.5,1.5,2.5,3.5,4.5])
+    fig.savefig('SentHist.png')
+
+
 
 if __name__ == '__main__':
     main()
